206e1dd16e8678298572c1c4e95267273350a1ba
Roman Marchenko <rmarchenko@azul.com>
2022-09-22 22:30:22 +0300

    Some test data

---
+++ MyTest.java
@@ -0,0 +1,13 @@
+// "sun.jnu.encoding"
+
+class MyTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // String prop = "sun.jnu.encoding";
+        // {
+        //     String value = System.getProperty(prop);
+        //     System.out.println(prop + "=" + value);
+        // }
+        // System.setProperty(prop, "cp-1252");
+    }
+}
